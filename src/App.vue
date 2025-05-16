06ff8f312626f316ad0d6d9e72ea9018b3da810c
tej-14-aswini <tejaswinihs95@gmail.com>
2025-05-15 17:14:52 -0700

    refactor  responsive

--- src/App.vue
+++ src/App.vue
@@ -30,10 +30,32 @@ provide("searchTerm", searchTerm);
 
 <style scoped lang="scss">
 .app-dimension-container {
-  width: 1440px;
-  height: 1024px;
+  width: 100vw;
+  min-width: 0;
+  height: 100vh;
+  min-height: 0;
   margin: 0 auto;
   background: #fff;
   box-sizing: border-box;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+}
+
+// Tablet and up
+@media (min-width: 600px) {
+  .app-dimension-container {
+    max-width: 100vw;
+    padding: 0 24px;
+  }
+}
+
+// Desktop and up
+@media (min-width: 1024px) {
+  .app-dimension-container {
+    max-width: 1440px;
+    margin: 0 auto;
+    padding: 0 40px;
+  }
 }
 </style>
